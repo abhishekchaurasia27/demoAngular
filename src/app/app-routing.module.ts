01db16885703648dd387425a5c9da4d18b7fb5e8
Abhishek Chaurasia <abhishekchaurasia27@gmail.com>
2020-08-02 17:32:37 +0530

    angular assingment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list-user', component: UserListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'demoProject';
+  loginUser: any = {};
+  loginSuccess: boolean;
+  users: User[];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.users = this.getUsers();
+  }
+
+  getUsers(): User[] {
+    return this.userService.getUsersFromData();
+  }
+
+  validateUser(user: User) {
+    if (this.userService.checkUser(user)) {
+      this.loginSuccess = true;
+    }
+  }
+
 }
